942d922294c8cd51b99264cf3cbd9af170022484
Vyshnav <vyshnav.m2023@vitstudent.ac.in>
2025-07-08 19:41:08 +0530

    ready to deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/NEWS-AGGREGATOR/',
   server: {
     port: 3000,
     proxy: {
       '/api': 'http://localhost:5000', // your backend runs on port 5000
     },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
 });
